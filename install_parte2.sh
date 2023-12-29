74e5b460f387fa750eae8ac2edc8a0a2642d7848
Eddy Galamba <137443771+glmbxecurity@users.noreply.github.com>
2023-12-29 12:40:10 +0100

    update install2

--- install_parte2.sh
+++ install_parte2.sh
@@ -14,12 +14,11 @@ clear
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 clear
 echo "Copiando ficheros .p10k.zsh y .zshrc en /root y /home/kali"
-sudo cp .p10k.zsh /root
-cp .p10k.zsh /home/kali
-sudo cp .zshrc /root
-cp .zshrc /home/kali
+sudo cp p10k.zsh /root/.p10k.zsh
+cp p10k.zsh /home/kali/.p10k.zsh
+sudo cp zshrc /root/.zshrc
+cp zshrc /home/kali/.zshrc
 source .zshrc
-sudo source .zshrc
 echo "Ficheros copiados correctamente"
 echo "INSTALACIÓN FINALIZADA!! ya puedes disfrutar de la personalización"
 
